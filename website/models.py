fc6ba300df773eae3a8aa657681fce46e90d9784
Carlos-860 <carlostimotheus5@gmail.com>
2024-12-07 12:49:23 +0200

    CRM-6 Use EmailField for email validation

--- website/models.py
+++ website/models.py
@@ -6,7 +6,7 @@ class Record(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    email = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
     phone = models.CharField(
         max_length=15,
         validators=[RegexValidator(
