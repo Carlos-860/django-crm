96d75cc4999c2f438bcd26cbe8c37977ce304e77
Carlos-860 <carlostimotheus5@gmail.com>
2024-12-08 16:59:35 +0200

    CRM-9 Create a new add Record form to handle the input for record details

--- website/forms.py
+++ website/forms.py
@@ -2,6 +2,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 from django.core.validators import RegexValidator
+from .models import Record
 
 class SignUpForm(UserCreationForm):
     email = forms.EmailField(
@@ -72,3 +73,18 @@ class SignUpForm(UserCreationForm):
         self.fields['password1'].help_text = '<ul class="form-text text-muted small"><li>Your password can\'t be too similar to your other personal information.</li><li>Your password must contain at least 8 characters.</li><li>Your password can\'t be a commonly used password.</li><li>Your password can\'t be entirely numeric.</li></ul>'
 
         self.fields['password2'].help_text = '<span class="form-text text-muted"><small>Enter the same password as before, for verification.</small></span>'
+
+# Create Add Record Form 
+class AddRecordForm(forms.ModelForm):
+	first_name = forms.CharField(required=True, max_length=50, widget=forms.widgets.TextInput(attrs={"placeholder":"First Name", "class":"form-control"}), label="", validators=[RegexValidator(r'^[a-zA-Z\s]*$', 'Only letters and spaces are allowed.')],)
+	last_name = forms.CharField(required=True,  max_length=50, widget=forms.widgets.TextInput(attrs={"placeholder":"Last Name", "class":"form-control"}), label="", validators=[RegexValidator(r'^[a-zA-Z\s]*$', 'Only letters and spaces are allowed.')],)
+	email = forms.CharField(required=True, max_length=100, widget=forms.widgets.TextInput(attrs={"placeholder":"Email", "class":"form-control"}), label="")
+	phone = forms.CharField(required=True, max_length=15, widget=forms.widgets.TextInput(attrs={"placeholder":"Phone", "class":"form-control"}), label="")
+	address = forms.CharField(required=True, max_length=100, widget=forms.widgets.TextInput(attrs={"placeholder":"Address", "class":"form-control"}), label="")
+	city = forms.CharField(required=True, max_length=50, widget=forms.widgets.TextInput(attrs={"placeholder":"City", "class":"form-control"}), label="")
+	state = forms.CharField(required=True, max_length=50, widget=forms.widgets.TextInput(attrs={"placeholder":"State", "class":"form-control"}), label="")
+	zip_code = forms.CharField(required=True, max_length=20, widget=forms.widgets.TextInput(attrs={"placeholder":"Zipcode", "class":"form-control"}), label="")
+
+	class Meta:
+		model = Record
+		exclude = ("user",)
